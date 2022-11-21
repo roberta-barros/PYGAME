60c5df27951dc5620fef9c09c95ef9b6cc253dbe
camilajafl <camilajafl@gmail.com>
2022-11-21 10:41:19 -0300

    tamanho

---
+++ config.py
@@ -0,0 +1,52 @@
+from os import path
+
+# Estabelece a pasta que contem as figuras e sons.
+IMG_DIR = path.join(path.dirname(__file__), 'assets', 'img')
+SND_DIR = path.join(path.dirname(__file__), 'assets', 'snd')
+FNT_DIR = path.join(path.dirname(__file__), 'assets', 'font')
+
+# Dados gerais do jogo.
+WIDTH = 480 # Largura da tela
+HEIGHT = 600 # Altura da tela
+FPS = 60 # Frames por segundo
+
+# Define tamanhos
+PECA1_WIDTH = 
+PECA1_HEIGHT = 
+
+PECA2_WIDTH = 
+PECA2_HEIGHT = 
+
+PECA3_WIDTH = 
+PECA3_HEIGHT = 
+
+PECA4_WIDTH = 
+PECA4_HEIGHT = 
+
+PECA5_WIDTH = 
+PECA5_HEIGHT = 
+
+PECA6_WIDTH = 
+PECA6_HEIGHT = 
+
+PECA7_WIDTH = 
+PECA7_HEIGHT = 
+
+PECA8_WIDTH = 
+PECA8_HEIGHT = 
+
+PECA9_WIDTH = 
+PECA9_HEIGHT = 
+
+# Define algumas variáveis com as cores básicas
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# Estados para controle do fluxo da aplicação
+INIT = 0
+GAME = 1
+QUIT = 2
