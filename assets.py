e423960d54be3401ac35062ff1e4e98872411169
roberta-barros <robertabarrost@gmail.com>
2022-11-23 08:47:26 -0300

    imagens

--- assets.py
+++ assets.py
@@ -3,27 +3,43 @@ import os
 from config import METEOR_WIDTH, METEOR_HEIGHT, SHIP_WIDTH, SHIP_HEIGHT, IMG_DIR, SND_DIR, FNT_DIR
 
 
-BACKGROUND = 'background'
-PECA_IMG = 'meteor_img'
-METEOR_IMG = 'meteor_img'
-SHIP_IMG = 'ship_img'
-SHIP_IMG = 'ship_img'
-BULLET_IMG = 'bullet_img'
-EXPLOSION_ANIM = 'explosion_anim'
+BACKGROUND1 = 'background1'
+BACKGROUND2 = 'background2'
+BACKGROUND3 = 'background3'
+PECA1_IMG = 'tetris2'
+PECA2_IMG = 'tetris2-removebg-preview'
+PECA3_IMG = 'tetris3'
+PECA4_IMG = 'tetris4'
+PECA5_IMG = 'tetris5'
+PECA6_IMG = 'tetris6'
+PECA7_IMG = 'tetris7'
+PECA8_IMG = 'tetris8'
+PECA9_IMG = 'tetris9'
 SCORE_FONT = 'score_font'
-BOOM_SOUND = 'boom_sound'
-DESTROY_SOUND = 'destroy_sound'
-PEW_SOUND = 'pew_sound'
-
+BACKGROUND_SOUND = 'background_sound'
+GAMEOVER_SOUND = 'gameover_sound'
 
 def load_assets():
     assets = {}
-    assets[BACKGROUND] = pygame.image.load(os.path.join(IMG_DIR, 'tetris1.png')).convert()
-    assets[METEOR_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'meteorBrown_med1.png')).convert_alpha()
-    assets[METEOR_IMG] = pygame.transform.scale(assets['meteor_img'], (METEOR_WIDTH, METEOR_HEIGHT))
-    assets[SHIP_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'playerShip1_orange.png')).convert_alpha()
-    assets[SHIP_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
-    assets[BULLET_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
+    assets[BACKGROUND1] = pygame.image.load(os.path.join(IMG_DIR, 'tetris1.png')).convert()
+    assets[PECA1_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'tetris2.png')).convert_alpha()
+    assets[PECA1_IMG] = pygame.transform.scale(assets['meteor_img'], (METEOR_WIDTH, METEOR_HEIGHT))
+    assets[PECA2_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'playerShip1_orange.png')).convert_alpha()
+    assets[PECA2_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
+    assets[PECA3_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
+    assets[PECA3_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
+    assets[PECA4_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
+    assets[PECA4_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
+    assets[PECA5_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
+    assets[PECA5_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
+    assets[PECA6_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
+    assets[PECA6_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
+    assets[PECA7_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
+    assets[PECA7_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
+    assets[PECA8_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
+    assets[PECA8_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
+    assets[PECA9_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
+    assets[PECA9_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
     explosion_anim = []
     for i in range(9):
         # Os arquivos de animação são numerados de 00 a 08
