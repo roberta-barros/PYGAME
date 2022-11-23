52ce587f49951226c4fd918175e255f2bf7fc44f
roberta-barros <robertabarrost@gmail.com>
2022-11-23 09:45:16 -0300

    som

--- assets.py
+++ assets.py
@@ -1,20 +1,19 @@
 import pygame
 import os
-from config import WIDTH, HEIGHT, PECA1_WIDTH, PECA1_HEIGHT, PECA2_WIDTH, PECA2_HEIGHT, PECA3_WIDTH, PECA3_HEIGHT, PECA4_WIDTH, PECA4_HEIGHT, PECA5_WIDTH,PECA5_HEIGHT,PECA6_WIDTH, PECA6_HEIGHT, PECA7_WIDTH, PECA7_HEIGHT, PECA8_WIDTH, PECA8_HEIGHT, PECA9_WIDTH, PECA9_HEIGHT, IMG_DIR, SND_DIR, FNT_DIR
+from config import WIDTH, HEIGHT, PECA_WIDTH, PECA_HEIGHT, IMG_DIR, SND_DIR, FNT_DIR
 
 
 BACKGROUND1 = 'background1'
 BACKGROUND2 = 'background2'
 BACKGROUND3 = 'background3'
 PECA1_IMG = 'tetris2'
-PECA2_IMG = 'tetris2-removebg-preview'
-PECA3_IMG = 'tetris3'
-PECA4_IMG = 'tetris4'
-PECA5_IMG = 'tetris5'
-PECA6_IMG = 'tetris6'
-PECA7_IMG = 'tetris7'
-PECA8_IMG = 'tetris8'
-PECA9_IMG = 'tetris9'
+PECA2_IMG = 'tetris3'
+PECA3_IMG = 'tetris4'
+PECA4_IMG = 'tetris5'
+PECA5_IMG = 'tetris6'
+PECA6_IMG = 'tetris7'
+PECA7_IMG = 'tetris8'
+PECA8_IMG = 'tetris9'
 SCORE_FONT = 'score_font'
 BACKGROUND_SOUND = 'background_sound'
 GAMEOVER_SOUND = 'gameover_sound'
@@ -25,27 +24,25 @@ def load_assets():
     assets[BACKGROUND2] = pygame.image.load(os.path.join(IMG_DIR, 'tetris_nivel2.png')).convert()
     assets[BACKGROUND3] = pygame.image.load(os.path.join(IMG_DIR, 'tetris_nivel3.png')).convert()
     assets[PECA1_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'tetris2.png')).convert_alpha()
-    assets[PECA1_IMG] = pygame.transform.scale(assets['meteor_img'], (METEOR_WIDTH, METEOR_HEIGHT))
-    assets[PECA2_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'playerShip1_orange.png')).convert_alpha()
-    assets[PECA2_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
-    assets[PECA3_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
-    assets[PECA3_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
-    assets[PECA4_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
-    assets[PECA4_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
-    assets[PECA5_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
-    assets[PECA5_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
-    assets[PECA6_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
-    assets[PECA6_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
-    assets[PECA7_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
-    assets[PECA7_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
-    assets[PECA8_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
-    assets[PECA8_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
-    assets[PECA9_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
-    assets[PECA9_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
+    assets[PECA1_IMG] = pygame.transform.scale(assets['tetris2'], (PECA_WIDTH, PECA_HEIGHT))
+    assets[PECA2_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'tetris3.png')).convert_alpha()
+    assets[PECA2_IMG] = pygame.transform.scale(assets['tetris3'], (PECA_WIDTH, PECA_HEIGHT))
+    assets[PECA3_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'tetris4.png')).convert_alpha()
+    assets[PECA3_IMG] = pygame.transform.scale(assets['tetris4'], (PECA_WIDTH, PECA_HEIGHT))
+    assets[PECA4_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'tetris5.png')).convert_alpha()
+    assets[PECA4_IMG] = pygame.transform.scale(assets['tetris5'], (PECA_WIDTH, PECA_HEIGHT))
+    assets[PECA5_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'tetris6.png')).convert_alpha()
+    assets[PECA5_IMG] = pygame.transform.scale(assets['tetris6'], (PECA_WIDTH, PECA_HEIGHT))
+    assets[PECA6_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'tetris7.png')).convert_alpha()
+    assets[PECA6_IMG] = pygame.transform.scale(assets['tetris7'], (PECA_WIDTH, PECA_HEIGHT))
+    assets[PECA7_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'tetris8.png')).convert_alpha()
+    assets[PECA7_IMG] = pygame.transform.scale(assets['tetris8'], (PECA_WIDTH, PECA_HEIGHT))
+    assets[PECA8_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'tetris9.png')).convert_alpha()
+    assets[PECA8_IMG] = pygame.transform.scale(assets['tetris9'], (PECA_WIDTH, PECA_HEIGHT))
     assets[SCORE_FONT] = pygame.font.Font(os.path.join(FNT_DIR, 'PressStart2P.ttf'), 28)
 
     # Carrega os sons do jogo
     pygame.mixer.music.load(os.path.join(SND_DIR, 'Tetris.wav'))
     pygame.mixer.music.set_volume(0.4)
-    assets[GAMEOVER_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'expl6.wav'))
+    assets[GAMEOVER_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'game-over.wav'))
     return assets
