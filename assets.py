b77bf73bd5bf2b7ef02ed3beff38d88f363708b6
roberta-barros <robertabarrost@gmail.com>
2022-11-23 09:33:45 -0300

    sons

--- assets.py
+++ assets.py
@@ -42,19 +42,10 @@ def load_assets():
     assets[PECA8_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
     assets[PECA9_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'laserRed16.png')).convert_alpha()
     assets[PECA9_IMG] = pygame.transform.scale(assets['ship_img'], (SHIP_WIDTH, SHIP_HEIGHT))
-    explosion_anim = []
-    for i in range(9):
-        # Os arquivos de animação são numerados de 00 a 08
-        filename = os.path.join(IMG_DIR, 'regularExplosion0{}.png'.format(i))
-        img = pygame.image.load(filename).convert()
-        img = pygame.transform.scale(img, (32, 32))
-        explosion_anim.append(img)
-    assets[EXPLOSION_ANIM] = explosion_anim
     assets[SCORE_FONT] = pygame.font.Font(os.path.join(FNT_DIR, 'PressStart2P.ttf'), 28)
 
     # Carrega os sons do jogo
-    pygame.mixer.music.load(os.path.join(SND_DIR, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
+    pygame.mixer.music.load(os.path.join(SND_DIR, 'Tetris.wav'))
     pygame.mixer.music.set_volume(0.4)
-    assets[BACKGROUND_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'Tetris.wav'))
     assets[GAMEOVER_SOUND] = pygame.mixer.Sound(os.path.join(SND_DIR, 'expl6.wav'))
     return assets
