cf0006c15863a69e0b6b5de3c47ce4b473b44f30
roberta-barros <robertabarrost@gmail.com>
2022-11-26 16:04:50 -0300

    versao

--- versao_1.py
+++ versao_1.py
@@ -5,41 +5,37 @@ import pygame
 pygame.init()
 
 # ----- Gera tela principal
-WIDTH = 1000
-HEIGHT = 700
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('tetris!')
+fps = 60
+Clock = pygame.time.Clock()
+ 
+W, H = 10, 20
+PECA = 38
+width = W * PECA
+height = H * PECA
+screen = pygame.display.set_mode((width, height))
+
+grid = [pygame.Rect(x * PECA, y * PECA, PECA, PECA) for x in range(W) for y in range(H)]
 
 # ----- Inicia estruturas de dados
 game = True
 
-# ----- Inicia assets
-image1 = pygame.image.load('tetris/tetris1.png').convert_alpha()  #colocar imagem
-                                                                   # .convert() acelerar
-                                                                   # .convert_alpha() quando a imagem possui transparência.
-image1 = pygame.transform.scale(image1, (180,320)) # diminuir o tamanho da imagem
-
-image2 = pygame.image.load('tetris/tetris2.png').convert_alpha()  #colocar imagem
-                                                                   # .convert() acelerar
-                                                                   # .convert_alpha() quando a imagem possui transparência.
-image2 = pygame.transform.scale(image2, (180,320)) # diminuir o tamanho da imagem
-
 # ===== Loop principal =====
 while game:
+    screen.fill((0, 0, 0)) # Preenche com a cor branca
     # ----- Trata eventos
     for event in pygame.event.get():
         # ----- Verifica consequências
         if event.type == pygame.QUIT:
             game = False
-
-    # ----- Gera saídas
-    window.fill((0, 0, 0))  # Preenche com a cor branca
-    window.blit(image1, (0, 0))
-    window.blit(image2, (10, 10))
+    
+    # Desenhar grades
+    [pygame.draw.rect(screen, (40, 40, 40), i_rect, 1) for i_rect in grid]
+  
+    pygame.display.flip()
+    Clock.tick(fps)
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
 
 # ===== Finalização =====
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
-
